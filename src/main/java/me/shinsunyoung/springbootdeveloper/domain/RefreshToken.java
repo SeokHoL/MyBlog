acce1276a06961e7b3691650e8e14e966e17f67d
seokholee <sukho8407@gmail.com>
2024-08-06 15:41:16 +0900

    jwt로 로그인 및 로그아웃 구현

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/domain/RefreshToken.java
@@ -0,0 +1,30 @@
+package me.shinsunyoung.springbootdeveloper.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false, unique = true)
+    private Long userId;
+
+    @Column(name = "refresh_token", nullable = false)
+    private String refreshToken;
+
+    public RefreshToken update(String newRefreshToken) {
+        this.refreshToken = newRefreshToken;
+        return this;
+    }
+}
+
+
